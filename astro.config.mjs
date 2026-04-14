185b341073e607ecab9d3802b87d0fbf5c9b041f
Diego Hincapie <difehian@bancolombia.com.co>
2026-04-14 17:01:31 -0500

    feat: se agregan configuraciones y mejoras en layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,7 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
-import tailwindcss from '@tailwindcss/vite';
-
 export default defineConfig({
-  site: 'https://tu-dominio.com',
-  integrations: [sitemap()],
-
-  vite: {
-    plugins: [tailwindcss()]
-  }
+  site: 'https://69deaa7288cac3166d4558db--storied-faloodeh-cebf92.netlify.app',
+  integrations: [sitemap()]
 });
