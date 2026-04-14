425e4b325b5db17a59173c8591a62ff3e102aaca
Diego Hincapie <difehian@bancolombia.com.co>
2026-04-14 17:07:34 -0500

    fix: se ajusta configuración de la app

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   site: 'https://69deaa7288cac3166d4558db--storied-faloodeh-cebf92.netlify.app',
-  integrations: [sitemap()]
+  integrations: [sitemap()],
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
