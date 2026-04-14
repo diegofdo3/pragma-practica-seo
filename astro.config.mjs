805d96f0d66697b4768dd869829977bcaa6422a1
Diego Hincapie <difehian@bancolombia.com.co>
2026-04-14 14:39:59 -0500

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,13 @@
+import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
+
+import tailwindcss from '@tailwindcss/vite';
+
+export default defineConfig({
+  site: 'https://tu-dominio.com',
+  integrations: [sitemap()],
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
+});
